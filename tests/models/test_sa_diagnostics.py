ed4f07f64c261ee601f69897cb6371d4c0ad906b
Andrey Vertiprahov <aversanta@gmail.com>
2023-04-12 14:33:48 +0500

    Additional tests.

--- tests/models/test_sa_diagnostics.py
+++ tests/models/test_sa_diagnostics.py
@@ -9,13 +9,24 @@
 from dataclasses import dataclass
 
 # NOC modules
-from noc.core.wf.diagnostic import DiagnosticConfig, diagnostic, DiagnosticState
+from noc.core.wf.diagnostic import (
+    DiagnosticConfig,
+    DiagnosticState,
+    diagnostic,
+    SNMP_DIAG,
+    CheckData,
+    Check,
+)
 
 
 @dataclass
 @diagnostic
 class Object(object):
-    diagnostics = {}
+    id = 10
+    diagnostics = {
+        "D2": {"diagnostic": "D2", "state": "failed", "reason": "1"},
+        "Access": {"diagnostic": "Access", "state": "failed", "reason": "2"},
+    }
 
     def iter_diagnostic_configs(self):
         """
@@ -23,9 +34,36 @@ class Object(object):
         :return:
         """
         yield DiagnosticConfig(diagnostic="D1")
+        yield DiagnosticConfig(diagnostic="D2")
+        ac = "C"
+        yield DiagnosticConfig(
+            SNMP_DIAG,
+            display_description="Check Device response by SNMP request",
+            checks=[Check(name="SNMPv1"), Check(name="SNMPv2c")],
+            blocked=ac == "C",
+            run_policy="F",
+            run_order="S",
+            discovery_box=True,
+            alarm_class="NOC | Managed Object | Access Lost",
+            alarm_labels=["noc::access::method::SNMP"],
+            reason="Blocked by AccessPreference" if ac == "C" else None,
+        )
+        yield DiagnosticConfig(
+            "Access",
+            dependent=["SNMP", "CLI", "HTTP"],
+            show_in_display=False,
+            alarm_class="NOC | Managed Object | Access Degraded",
+        )
 
 
 def test_set_state():
     o = Object()
+    assert o.diagnostic.D2.state == DiagnosticState.failed
     o.diagnostic.set_state("D1", DiagnosticState.enabled)
     assert o.diagnostic.D1.state == DiagnosticState.enabled
+    o.diagnostic.update_checks([CheckData(name="SNMPv1", status=False)])
+    assert o.diagnostic.SNMP.state == DiagnosticState.failed
+    o.diagnostic.update_checks([CheckData(name="SNMPv1", status=False), CheckData(name="SNMPv1", status=True)])
+    assert o.diagnostic.SNMP.state == DiagnosticState.enabled
+    assert o.diagnostic.Access.state == DiagnosticState.enabled
+    assert o.diagnostics["Access"]["state"] == "enabled"
